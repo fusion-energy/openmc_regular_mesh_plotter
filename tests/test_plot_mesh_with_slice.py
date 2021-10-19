784fb8971112ef614b429f99f818196c6c1585b6
Jonathan <mail@jshimwell.com>
2021-10-19 02:13:42 +0100

    added future test

---
+++ tests/test_plot_mesh_with_slice.py
@@ -0,0 +1,22 @@
+
+# todo test the combination of mesh and stl slice
+# stl_slice = plot_stl_slice(
+#     stl_or_mesh='steel.stl',
+#     plane_origin = None,
+#     plane_normal = [0, 0, 1],
+#     rotate_plot = 0,
+#     filename='slice.png'
+# )
+
+# extent = get_tally_extent(my_tally)
+# print(extent)
+# plot_mesh(
+#     extent=extent,
+#     values= result,
+#     scale=None,  # LogNorm(),
+#     vmin=None,
+#     label="picosievert / cm / pulse",
+#     base_plt=stl_slice,
+#     filename= 'test.png',
+# #     vmin=1e6,
+# )
