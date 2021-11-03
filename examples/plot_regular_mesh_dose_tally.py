87c0139fc557ff3b10427d2511150b391bcbe6dd
Jonathan <mail@jshimwell.com>
2021-11-03 21:30:52 +0000

    examples now run

--- examples/plot_regular_mesh_dose_tally.py
+++ examples/plot_regular_mesh_dose_tally.py
@@ -10,15 +10,15 @@ my_tally = statepoint.get_tally(name="neutron_effective_dose_on_2D_mesh_xy")
 # creates a plot of the mesh tally
 my_plot = rmp.plot_regular_mesh_dose_tally(
     tally=my_tally,  # the openmc tally object to plot, must be a 2d mesh tally
-    filename='plot_regular_mesh_dose_tally.png',  # the filename of the picture file saved
+    filename="plot_regular_mesh_dose_tally.png",  # the filename of the picture file saved
     scale=None,  # LogNorm(),
     vmin=None,
     label="",
     x_label="X [cm]",
     y_label="Y [cm]",
-    rotate_plot = 0,
-    required_units='picosievert cm **2 / simulated_particle',
-    source_strength = None,
+    rotate_plot=0,
+    required_units="picosievert cm **2 / simulated_particle",
+    source_strength=None,
 )
 
 # displays the plot
