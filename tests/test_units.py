c20f1dfe1933c5f502edbf5155026103b7b7edb0
Jonathan Shimwell <mail@jshimwell.com>
2023-09-11 10:15:25 +0100

    added test for 2d mesh tally

--- tests/test_units.py
+++ tests/test_units.py
@@ -45,7 +45,7 @@ def model():
     return model
 
 
-def test_plot_mesh_tally(model):
+def test_plot_3d_mesh_tally(model):
 
     geometry = model.geometry
 
@@ -59,8 +59,9 @@ def test_plot_mesh_tally(model):
     model.tallies = tallies
 
     sp_filename = model.run()
-    statepoint = openmc.StatePoint(sp_filename)
-    tally_result = statepoint.get_tally(name="mesh-tal")
+    with openmc.StatePoint(sp_filename) as statepoint:
+        tally_result = statepoint.get_tally(name="mesh-tal")
+
 
     plot = plot_mesh_tally(
         tally=tally_result, basis="xy", slice_index=29  # max value of slice selected
@@ -102,3 +103,63 @@ def test_plot_mesh_tally(model):
     assert plot.get_xlim() == (-1000.0, 500)  # note that units are in mm
     assert plot.get_ylim() == (-3000.0, 3500.0)
     plot.figure.savefig("z.png")
+
+
+def test_plot_2d_mesh_tally(model):
+
+    geometry = model.geometry
+
+    mesh = openmc.RegularMesh().from_domain(
+        geometry, dimension=[1, 20, 30])
+    mesh_filter = openmc.MeshFilter(mesh)
+    mesh_tally = openmc.Tally(name="mesh-tal")
+    mesh_tally.filters = [mesh_filter]
+    mesh_tally.scores = ["flux"]
+    tallies = openmc.Tallies([mesh_tally])
+
+    model.tallies = tallies
+
+    sp_filename = model.run()
+    with openmc.StatePoint(sp_filename) as statepoint:
+        tally_result = statepoint.get_tally(name="mesh-tal")
+
+    plot = plot_mesh_tally(
+        tally=tally_result, basis="yz", slice_index=29  # max value of slice selected
+    )
+    # # axis_units defaults to cm
+    # assert plot.xaxis.get_label().get_text() == "x [cm]"
+    # assert plot.yaxis.get_label().get_text() == "y [cm]"
+    # assert plot.get_xlim() == (-100.0, 50)
+    # assert plot.get_ylim() == (-200.0, 250.0)
+
+    # plot = plot_mesh_tally(
+    #     tally=tally_result,
+    #     basis="yz",
+    #     axis_units="m",
+    #     slice_index=9,  # max value of slice selected
+    #     value="std_dev",
+    # )
+    # plot.figure.savefig("x.png")
+    # assert plot.xaxis.get_label().get_text() == "y [m]"
+    # assert plot.yaxis.get_label().get_text() == "z [m]"
+    # assert plot.get_xlim() == (-2.0, 2.5)  # note that units are in m
+    # assert plot.get_ylim() == (-3.0, 3.5)
+
+    # plot = plot_mesh_tally(
+    #     tally=tally_result,
+    #     basis="xz",
+    #     slice_index=19,  # max value of slice selected
+    #     axis_units="mm",
+    #     score="flux",
+    #     value="mean",
+    #     outline=True,
+    #     geometry=geometry,
+    #     outline_by="material",
+    #     colorbar_kwargs={"label": "neutron flux"},
+    #     norm=LogNorm(vmin=1e-6, vmax=max(tally_result.mean.flatten())),
+    # )
+    # assert plot.xaxis.get_label().get_text() == "x [mm]"
+    # assert plot.yaxis.get_label().get_text() == "z [mm]"
+    # assert plot.get_xlim() == (-1000.0, 500)  # note that units are in mm
+    # assert plot.get_ylim() == (-3000.0, 3500.0)
+    # plot.figure.savefig("z.png")
